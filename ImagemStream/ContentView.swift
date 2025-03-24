8b75d9a9ea559ce06db6fdc38d9c5e00282f8485
rluispdev <rluis@icloud.com>
2025-03-24 19:41:56 -0300

    Inicializando repositório e ignorando Secrets.xcconfig

---
+++ ImagemStream/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImagemStream
+//
+//  Created by Rafael Gonzaga on 3/24/25.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImagemStream/ImagemStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagemStreamApp.swift
+//  ImagemStream
+//
+//  Created by Rafael Gonzaga on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagemStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
